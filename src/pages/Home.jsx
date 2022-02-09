e9dd2dc49e5c690d1d7a98451cd2bff89215d1c8
Eddy Ruiz <eddy@eddys-mbp.lan>
2022-02-09 11:36:58 -0500

    pushing

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import UsersResults from "../components/users/UsersResults";
+import UserSearch from "../components/users/UserSearch";
+
+const Home = () => {
+  return (
+    <>
+      <UserSearch />
+      <UsersResults />
+    </>
+  );
+};
+
+export default Home;
